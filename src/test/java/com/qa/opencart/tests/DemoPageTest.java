364306c924b9f35ad6ef82fb827c23c65e585640
Anusha Bhat <programmingwithAnusha@gmail.com>
2023-03-20 11:48:12 +1100

    adding DemoPageTest.Java

---
+++ src/test/java/com/qa/opencart/tests/DemoPageTest.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.tests;
+
+public class DemoPageTest {
+
+	int a;
+	public int MethodTotest() {
+	if(a!=0) {
+		System.out.println(a);
+	}
+	else {
+		a=10;
+	}
+	return a;
+}
+	
+}
